cf4970813edb279e1912274f16022b9871c42620
Oskar Kupski <oskar.kupski@gmail.com>
2021-02-13 11:48:02 +0100

    Extracted controller logic to custom hook

--- src/TemperatureCard/TemperatureCard.service.ts
+++ src/TemperatureCard/TemperatureCard.service.ts
@@ -2,11 +2,14 @@ import { injectable } from "tsyringe";
 
 @injectable()
 export default class TemperatureCardService {
-  fromCelsiusToFahrenheit(celsiusTemperature: number) {
+  fromCelsiusToFahrenheit(celsiusTemperature?: number) {
+    if (celsiusTemperature === undefined) return undefined;
+
     return parseFloat((celsiusTemperature * 1.8 + 32).toFixed(1));
   }
 
-  getTemperatureDescription(celsiusTemperature: number) {
+  getTemperatureDescription(celsiusTemperature?: number) {
+    if (celsiusTemperature === undefined) return undefined;
     if (celsiusTemperature < 0) return "Winter has come";
     if (celsiusTemperature < 7) return "Spring is almost here";
     if (celsiusTemperature < 15) return "Spring has started";
